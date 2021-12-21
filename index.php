74adca7de771593a855a83accee70ee53a31f93d
Alongkod113 <80100652+Alongkod113@users.noreply.github.com>
2021-12-21 21:27:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,57 +19,58 @@
     .br {
         background-color: aquamarine
     }
-    .br1{
-        background:rgba(97, 121, 255, 0.822) url("https://htmlcheatsheet.com/images/logo-css.png") no-repeat fixed 0 0;
+
+    .br1 {
+        background: rgba(97, 121, 255, 0.822) url("https://htmlcheatsheet.com/images/logo-css.png") no-repeat fixed 0 0;
     }
 </style>
 
 <body>
+
     <body class="br"></body>
 
     <div class="container">
         <p><img src="https://kasikornresearch.com/SiteCollectionDocuments/analysis/business/food/resturant_Banner2.jpg"
                 alt="รูปภาพ" height="450" width="1285" vspace="20" hspace="10">
-                <h3 class="br1">Ros Dee Kitchen</h3>
-                
+        <h3 class="br1">Ros Dee Kitchen</h3>
+
         <form class="row g-3">
             <div class="col-md-6">
-                
-                <label for="ShopName" class="form-label"><u>ShopName</u></label>
-                <input type="text" class="form-control" id="ShopName">
+                <label for="Shop" class="form-label">ShopName</label>
+                <input type="text" class="form-control" id="Shop">
             </div>
             <div class="col-md-6">
-                <label for="hfRowIndex" class="form-label"></label>
-                <input type="hidden" class="form-control" id="hfRowIndex">
+                <label for="RowIndex" class="form-label"> </label>
+                <input type="hidden" class="form-control" id="RowIndex">
             </div>
             <div class="col-md-6">
-                <label for="Idfood" class="form-label"><u> ID</u></label>
-                <input type="text" class="form-control" id="Idfood" value="1">
+                <label for="Id" class="form-label">ID</label>
+                <input type="text" class="form-control" id="Id" value="1">
             </div>
             <div class="col-6">
-                <label for="Namefood" class="form-label"><u>Name</u> </label>
-                <input type="text" class="form-control" id="Namefood" placeholder="">
+                <label for="Name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="Name" placeholder="">
             </div>
             <div class="col-6">
-                <label for="pricefood" class="form-label"><u> Price</u></label>
-                <input type="number" class="form-control" id="pricefood" placeholder=" ">
+                <label for="price" class="form-label">Price</label>
+                <input type="number" class="form-control" id="price" placeholder=" ">
             </div>
             <div class="col-md-6">
-                <label for="link" class="form-label"><u>link</u> </label>
+                <label for="link" class="form-label">link</label>
                 <input type="text" class="form-control" id="link">
             </div>
             <div class="col-4">
-                <button type="button" class="btn btn-success" id='btnAdd'>add</button>
+                <button type="button" class="btn btn-success" id='btnadd'>add</button>
             </div>
             <div class="col-4">
-                <button type="button" class="btn btn-primary" id='btnUpdate' style="display: none;">Update</button>
+                <button type="button" class="btn btn-primary" id='btnupdate' style="display: none;">Update</button>
             </div>
             <div class="col-4">
-                <button type="button" class="btn btn-danger" id='btnClear'>delete data</button>
+                <button type="button" class="btn btn-danger" id='btnclear'>delete data</button>
             </div>
         </form>
     </div>
-    <!--ฟอร์ม-->
+    <!--ฟอม-->
     <table id="tblAll" class="table table-striped" style="margin-top:23px">
         <thead>
             <tr>
@@ -89,47 +90,47 @@
 
 <script>
     $(function () {
-        $('#btnAdd').on('click', function () {
-            var ShopName, namefood, pricefood, link, Idfood;
-            ShopName = $("#ShopName").val();
-            Idfood = $("#Idfood").val();
-            Namefood = $("#Namefood").val();
-            pricefood = $("#pricefood").val();
+        $('#btnadd').on('click', function () {
+            var Shop, name, price, link, Id;
+            Shop = $("#Shop").val();
+            Id = $("#Id").val();
+            Name = $("#Name").val();
+            price = $("#price").val();
             link = $("#link").val();
 
             var edit = "<a class='edit' href='JavaScript:void(0);' >Edit</a>";
             var del = "<a class='delete' href='JavaScript:void(0);'>Delete</a>";
 
-            if (Namefood == "" || pricefood == "" || ShopName == " " || link == " ") {
+            if (Name == "" || price == "" || Shop == " " || link == " ") {
                 alert("กรุณากรอกข้อมูล!");
             } else {
-                var table = "<tr><td>" + Idfood + "</td><td>" + ShopName + "</td><td>" + Namefood + "</td><td>" + pricefood + "</td><td>" + link + "</td><td>" + edit + "</td><td>" + del + "</td></tr>";
+                var table = "<tr><td>" + Id + "</td><td>" + Shop + "</td><td>" + Name + "</td><td>" + price + "</td><td>" + link + "</td><td>" + edit + "</td><td>" + del + "</td></tr>";
                 $("#tblAll").append(table);
             }
-            Idfood = $("#Idfood").val("");
-            ShopName = $("#ShopName").val();
-            Namefood = $("#Namefood").val("");
-            pricefood = $("#pricefood").val("");
+            Id = $("#Id").val("");
+            Shop = $("#Shop").val();
+            Name = $("#Name").val("");
+            price = $("#price").val("");
             link = $("#link").val();
             Clear();
         });
 
-        $('#btnUpdate').on('click', function () {
-            var ShopName, namefood, pricefood, link, Idfood;
-            ShopName = $("#ShopName").val();
-            Idfood = $("#Idfood").val();
-            Namefood = $("#Namefood").val();
-            pricefood = $("#pricefood").val();
+        $('#btnupdate').on('click', function () {
+            var Shop, name, price, link, Id;
+            Shop = $("#Shop").val();
+            Id = $("#Id").val();
+            Name = $("#Name").val();
+            price = $("#price").val();
             link = $("#link").val();
 
-            $('#tblAll tbody tr').eq($('#hfRowIndex').val()).find('td').eq(0).html(Idfood);
-            $('#tblAll tbody tr').eq($('#hfRowIndex').val()).find('td').eq(1).html(ShopName);
-            $('#tblAll tbody tr').eq($('#hfRowIndex').val()).find('td').eq(2).html(Namefood);
-            $('#tblAll tbody tr').eq($('#hfRowIndex').val()).find('td').eq(3).html(pricefood);
-            $('#tblAll tbody tr').eq($('#hfRowIndex').val()).find('td').eq(3).html(link);
+            $('#tblAll tbody tr').eq($('#RowIndex').val()).find('td').eq(0).html(Id);
+            $('#tblAll tbody tr').eq($('#RowIndex').val()).find('td').eq(1).html(Shop);
+            $('#tblAll tbody tr').eq($('#RowIndex').val()).find('td').eq(2).html(Name);
+            $('#tblAll tbody tr').eq($('#RowIndex').val()).find('td').eq(3).html(price);
+            $('#tblAll tbody tr').eq($('#RowIndex').val()).find('td').eq(3).html(link);
 
-            $('#btnAdd').show();
-            $('#btnUpdate').hide();
+            $('#btnadd').show();
+            $('#btnupdate').hide();
             Clear();
         });
 
@@ -141,30 +142,30 @@
             }
         });
 
-        $('#btnClear').on('click', function () {
-            Clear();
+        $('#btnclear').on('click', function () {
+            clear();
         });
 
         $("#tblAll").on("click", ".edit", function (e) {
             var row = $(this).closest('tr');
             $('#hfRowIndex').val($(row).index());
             var td = $(row).find("td");
-            $('#ShopName').val($(td).eq(1).html());
-            $('#Idfood').val($(td).eq(0).html());
-            $('#Namefood').val($(td).eq(2).html());
-            $('#pricefood').val($(td).eq(3).html());
+            $('#Shop').val($(td).eq(1).html());
+            $('#Id').val($(td).eq(0).html());
+            $('#Name').val($(td).eq(2).html());
+            $('#price').val($(td).eq(3).html());
             $('#link').val($(td).eq(4).html());
-            $('#btnAdd').hide();
-            $('#btnUpdate').show();
+            $('#btnadd').hide();
+            $('#btnupdate').show();
         });
     });
-    function Clear() {
-        $("#ShopName").val("");
-        $("#Idfood").val("");
-        $("#Namefood").val("");
-        $("#pricefood").val("");
+    function clear() {
+        $("#Shop").val("");
+        $("#Id").val("");
+        $("#Name").val("");
+        $("#price").val("");
         $("#link").val("");
-        $("#hfRowIndex").val("");
+        $("#RowIndex").val("");
     }
 </script>
 
